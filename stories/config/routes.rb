77264406c9808c127d88ec94bd7a9f5459c0fb0e
codecademy <ccuser@codecademy.com>
2020-11-26 13:34:14 +0000

    created the signups pages and form

--- stories/config/routes.rb
+++ stories/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
-  root 'page#home'
+  root 'pages#home'
+
   get '/about' => 'pages#about'
+  ressources :signups
+
+  get '/thanks' => 'pages#thanks'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
